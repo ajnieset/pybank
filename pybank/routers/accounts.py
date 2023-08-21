ee96bdcb78afae4601079fada2e7498e9dd16a06
Austin Nieset <ajnieset@gmail.com>
2023-08-20 22:00:06 -0400

    wrapped create account in try catch

--- pybank/routers/accounts.py
+++ pybank/routers/accounts.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import Response
 from sqlalchemy.orm import Session
 
 from ..db import get_db
 from ..models import Account
-from ..schemas import Account as AccountData
+from ..schemas import AccountBase
 
 
 router = APIRouter(prefix="/accounts")
@@ -13,7 +14,10 @@ def get_account(account_id:int, db: Session = Depends(get_db)):
     return Account.get_by_id(account_id, db)
 
 @router.post("")
-def create_account(db: Session = Depends(get_db)):
-    data = AccountData(id=1, balance=0.00, user_id=1, User=None)
-    Account.create_account(data, db)
-    return "", 200
+def create_account(data: AccountBase, db: Session = Depends(get_db)):
+    try:
+        Account.create_account(data, db)
+    except Exception as e:
+        print(e)
+        return Response(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return Response(status_code=status.HTTP_201_CREATED)
